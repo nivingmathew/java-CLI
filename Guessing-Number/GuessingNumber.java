09173e155e204b1cbcd40dbdc3edf027bdd96041
nivin <nivingmathew@gmail.com>
2026-05-27 22:29:43 +0530

    refactor: consolidate duplicate game methods

--- Guessing-Number/GuessingNumber.java
+++ Guessing-Number/GuessingNumber.java
@@ -1,112 +1,62 @@
 
-//import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GuessingNumber {
 
+    private static final int EASY_MAX = 100;
+    private static final int HARD_MAX = 500;
+
     public static void main(String[] args) {
         int secretNumber;
         String checking;
-        
 
         System.out.println("-----Welcome to Number Guessing Game-----");
 
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\nSelect the difficulty level\n1-Easy\n2-Hard");
+            System.out.println("\nSelect the difficulty level\n1-Easy (1-100)\n2-Hard (1-500)");
 
             int level = sc.nextInt();
 
-            if (level != 1 && level != 2) {
-                System.out.println("\nWrong input");
-                System.out.println("Do you want to continue(Y/N)");
-                checking = sc.next();
-                if (checking.equalsIgnoreCase("y")) {
-                    continue;
-                } else if (checking.equalsIgnoreCase("n")) {
-                    System.out.println("\n-----See you later-----");
-                    break;
-                } else {
-                    System.out.println("\nWrong Input Again\nBe serious. BYE");
-                    break;
-                }
+            if (level == 1) {
+                secretNumber = ThreadLocalRandom.current().nextInt(1, EASY_MAX + 1);
+                playGame(secretNumber, sc, EASY_MAX);
+            } else if (level == 2) {
+                secretNumber = ThreadLocalRandom.current().nextInt(1, HARD_MAX + 1);
+                playGame(secretNumber, sc, HARD_MAX);
+            } else {
+                System.out.println("\nInvalid level. Try again.");
+                continue;
             }
-            
-            switch (level) {
-                case 1:
-                    secretNumber = ThreadLocalRandom.current().nextInt(1, 101);
-                    easyGame(secretNumber, sc);
-                    break;
 
-                case 2:
-                    secretNumber = ThreadLocalRandom.current().nextInt(1, 501);
-                    hardGame(secretNumber, sc);
-                    break;
-
-                default:
-                    break;
-            }
             System.out.println("Do you want to continue the game(Y/N)");
             checking = sc.next();
-            if(checking.equalsIgnoreCase("y")) {
-                continue;
-            } else {
-                checking.equalsIgnoreCase("n");
+            if (!checking.equalsIgnoreCase("y")) {
+                System.out.println("Exiting...");
+                sc.close();
                 break;
             }
-            
         }
     }
 
-    public static void easyGame(int secretNumber, Scanner sc) {
-        int value;
-        System.out.println("\nEnter your guess(1 - 100): ");
-        value = sc.nextInt();
+    private static void playGame(int secret, Scanner sc, int maxRange) {
+        int attempts = 0;
+        System.out.println("\nEnter your guess(1 - " + maxRange + ")");
         while (true) {
-            if (value < 101 && value > 0) {
-                if(secretNumber == value) {
-                    System.out.println("Found the number");
-                    break;
-                } else {
-                    if(value>secretNumber) {
-                        System.out.println("Lower");
-                    } else {
-                        System.out.println("Higher");
-                    }
-                    System.out.println("Enter another guess");
-                    value = sc.nextInt();
-                }
-            } else {
-                System.out.println("Enter a valid number");
-                value = sc.nextInt();
-            }
-        }
-    }
+            int guess = sc.nextInt();
+            attempts++;
 
-    static void hardGame(int secretNumber, Scanner sc) {
-        int value;
-        System.out.println("\nEnter your guess(1 - 500): ");
-        value = sc.nextInt();
-        while (true) {
-            if (value < 501 && value > 0) {
-                if(secretNumber == value) {
-                    System.out.println("Found the number");
-                    break;
-                } else {
-                    if(value>secretNumber) {
-                        System.out.println("Lower");
-                    } else {
-                        System.out.println("Higher");
-                    }
-                    System.out.println("Enter another guess");
-                    value = sc.nextInt();
-                }
+            if (guess < 1 || guess > maxRange) {
+                System.out.println("Out of range. Enter a number between 1 and " + maxRange + ":");
+            } else if (guess == secret) {
+                System.out.println("Correct! You got it in " + attempts + " attempt(s)!");
+                break;
             } else {
-                System.out.println("Enter a valid number");
-                value = sc.nextInt();
+                System.out.println(guess > secret ? "High, Try lower:" : "Low, Try higher:");
             }
         }
+
     }
 }
